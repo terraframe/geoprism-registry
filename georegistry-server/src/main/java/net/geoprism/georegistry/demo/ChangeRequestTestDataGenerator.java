6167edc677467a968909f6038a3a1c555c603e49
Justin Smethie <jsmethie@terraframe.com>
2019-02-13 12:44:44 -0700

    Governance Test Data

--- georegistry-server/src/main/java/net/geoprism/georegistry/demo/ChangeRequestTestDataGenerator.java
+++ georegistry-server/src/main/java/net/geoprism/georegistry/demo/ChangeRequestTestDataGenerator.java
@@ -37,86 +37,115 @@ public class ChangeRequestTestDataGenerator
   {
     ChangeRequestTestDataGenerator.build();
   }
-  
+
   @Request
   private static void build()
   {
     ChangeRequestTestDataGenerator.deleteAllActions();
     ChangeRequestTestDataGenerator.deleteAllChangeRequests();
-    
+
     buildInTransaction();
   }
-  @Transaction
+
   private static void buildInTransaction()
   {
-    genChangeRequest("CR1", Instant.now().minus(3, ChronoUnit.DAYS));
-    genChangeRequest("CR2", Instant.now().minus(2, ChronoUnit.DAYS));
-    genChangeRequest("CR3", Instant.now().minus(1, ChronoUnit.DAYS));
+    genChangeRequest("CR1", Instant.now().minus(3, ChronoUnit.DAYS), true, false);
+
+    try
+    {
+      Thread.sleep(1500);
+    }
+    catch (InterruptedException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    genChangeRequest("CR2", Instant.now().minus(2, ChronoUnit.DAYS), false, true);
+
+    try
+    {
+      Thread.sleep(1500);
+    }
+    catch (InterruptedException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    genChangeRequest("CR3", Instant.now().minus(1, ChronoUnit.DAYS), false, false);
   }
-  
-  private static void genChangeRequest(String genKey, Instant when)
+
+  @Transaction
+  private static void genChangeRequest(String genKey, Instant when, boolean includeRemove, boolean includeAdd)
   {
     GeoObject goNewChild = ServiceFactory.getAdapter().newGeoObjectInstance("Cambodia_District");
     goNewChild.setCode(genKey + "_CODE");
     goNewChild.setLocalizedDisplayLabel(genKey + "_LABEL");
-    goNewChild.setWKTGeometry("POLYGON ((10000 10000, 12300 40000, 16800 50000, 12354 60000, 13354 60000, 17800 50000, 13300 40000, 11000 10000, 10000 10000))");
-    
+    goNewChild.setWKTGeometry("MULTIPOLYGON (((10000 10000, 12300 40000, 16800 50000, 12354 60000, 13354 60000, 17800 50000, 13300 40000, 11000 10000, 10000 10000)))");
+
     GeoObject testAddChildParent = ServiceFactory.getUtilities().getGeoObjectByCode("855 01", "Cambodia_Province");
     GeoObject testAddChild = ServiceFactory.getUtilities().getGeoObjectByCode("855 0109", "Cambodia_District");
 
     List<AbstractActionDTO> actions = new ArrayList<AbstractActionDTO>();
 
     /*
-     *  Add Child
-     */
-    AddChildActionDTO addChild = new AddChildActionDTO();
-    addChild.setChildId(testAddChild.getUid());
-    addChild.setChildTypeCode(testAddChild.getType().getCode());
-    addChild.setParentId(testAddChildParent.getUid());
-    addChild.setParentTypeCode(testAddChildParent.getType().getCode());
-    addChild.setHierarchyCode(LocatedIn.class.getSimpleName());
-    addChild.setCreateActionDate(Date.from(when.minus(10, ChronoUnit.HOURS)));
-    
-    String addChildJson = addChild.toJSON().toString();
-    String addChildJson2 = AbstractActionDTO.parseAction(addChildJson).toJSON().toString();
-    actions.add(addChild);
-    
-    /*
      * Remove Child
      */
-    RemoveChildActionDTO removeChild = new RemoveChildActionDTO();
-    removeChild.setChildId(testAddChild.getUid());
-    removeChild.setChildTypeCode(testAddChild.getType().getCode());
-    removeChild.setParentId(testAddChildParent.getUid());
-    removeChild.setParentTypeCode(testAddChildParent.getType().getCode());
-    removeChild.setHierarchyCode(LocatedIn.class.getSimpleName());
-    removeChild.setCreateActionDate(Date.from(when.minus(9, ChronoUnit.HOURS)));
-    
-    String removeChildJson = removeChild.toJSON().toString();
-    String removeChildJson2 = AbstractActionDTO.parseAction(removeChildJson).toJSON().toString();
-    actions.add(removeChild);
+    if (includeRemove)
+    {
+      RemoveChildActionDTO removeChild = new RemoveChildActionDTO();
+      removeChild.setChildId(testAddChild.getUid());
+      removeChild.setChildTypeCode(testAddChild.getType().getCode());
+      removeChild.setParentId(testAddChildParent.getUid());
+      removeChild.setParentTypeCode(testAddChildParent.getType().getCode());
+      removeChild.setHierarchyCode(LocatedIn.class.getSimpleName());
+      removeChild.setCreateActionDate(Date.from(when.minus(9, ChronoUnit.HOURS)));
+
+      String removeChildJson = removeChild.toJSON().toString();
+      String removeChildJson2 = AbstractActionDTO.parseAction(removeChildJson).toJSON().toString();
+      actions.add(removeChild);
+    }
 
     /*
-     *  Create a new GeoObject
+     * Add Child
+     */
+    if (includeAdd)
+    {
+      AddChildActionDTO addChild = new AddChildActionDTO();
+      addChild.setChildId(testAddChild.getUid());
+      addChild.setChildTypeCode(testAddChild.getType().getCode());
+      addChild.setParentId(testAddChildParent.getUid());
+      addChild.setParentTypeCode(testAddChildParent.getType().getCode());
+      addChild.setHierarchyCode(LocatedIn.class.getSimpleName());
+      addChild.setCreateActionDate(Date.from(when.minus(10, ChronoUnit.HOURS)));
+
+      String addChildJson = addChild.toJSON().toString();
+      String addChildJson2 = AbstractActionDTO.parseAction(addChildJson).toJSON().toString();
+      actions.add(addChild);
+    }
+
+    /*
+     * Create a new GeoObject
      */
     CreateGeoObjectActionDTO create = new CreateGeoObjectActionDTO();
     create.setGeoObject(goNewChild.toJSON());
     create.setCreateActionDate(Date.from(when.minus(8, ChronoUnit.HOURS)));
-    
+
     String createJson = create.toJSON().toString();
     String createJson2 = AbstractActionDTO.parseAction(createJson).toJSON().toString();
     actions.add(create);
 
     /*
-     *  Update the previously created GeoObject
+     * Update the previously created GeoObject
      */
     final String NEW_DISPLAY_LABEL = genKey + "_NEW_DISPLAY_LABEL";
     goNewChild.setLocalizedDisplayLabel(NEW_DISPLAY_LABEL);
-    
+
     UpdateGeoObjectActionDTO update = new UpdateGeoObjectActionDTO();
     update.setGeoObject(goNewChild.toJSON());
     update.setCreateActionDate(Date.from(when.minus(7, ChronoUnit.HOURS)));
-    
+
     String updateJson = update.toJSON().toString();
     String updateJson2 = AbstractActionDTO.parseAction(updateJson).toJSON().toString();
     actions.add(update);
@@ -126,13 +155,13 @@ public class ChangeRequestTestDataGenerator
 
     submitChangeRequest(sActions);
   }
-  
+
   private static void submitChangeRequest(String sJson)
   {
     ChangeRequest cr = new ChangeRequest();
     cr.addApprovalStatus(AllGovernanceStatus.PENDING);
     cr.apply();
-    
+
     List<AbstractActionDTO> actionDTOs = AbstractActionDTO.parseActions(sJson);
 
     for (AbstractActionDTO actionDTO : actionDTOs)
@@ -140,35 +169,35 @@ public class ChangeRequestTestDataGenerator
       AbstractAction ra = AbstractAction.dtoToRegistry(actionDTO);
       ra.addApprovalStatus(AllGovernanceStatus.PENDING);
       ra.apply();
-      
+
       cr.addAction(ra).apply();
     }
   }
-  
-  
+
   /**
-   * These were copied/pasted from TestDataSet because this code is to be run as part of a demo.
+   * These were copied/pasted from TestDataSet because this code is to be run as
+   * part of a demo.
    */
   @Request
   public static void deleteAllActions()
   {
     AbstractActionQuery aaq = new AbstractActionQuery(new QueryFactory());
-    
+
     OIterator<? extends AbstractAction> it = aaq.getIterator();
-    
+
     while (it.hasNext())
     {
       it.next().delete();
     }
   }
-  
+
   @Request
   public static void deleteAllChangeRequests()
   {
     ChangeRequestQuery crq = new ChangeRequestQuery(new QueryFactory());
-    
+
     OIterator<? extends ChangeRequest> it = crq.getIterator();
-    
+
     while (it.hasNext())
     {
       it.next().delete();
