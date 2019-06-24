d4d2c7826069efbc8d76f1039784468457463c37
Justin Smethie <jsmethie@terraframe.com>
2019-06-24 15:39:32 -0600

    https://terraframe.atlassian.net/browse/CGR-330

---
+++ georegistry-server/src/main/java-gen/stub/net/geoprism/registry/InvalidMasterListCodeException.java
@@ -0,0 +1,27 @@
+package net.geoprism.registry;
+
+public class InvalidMasterListCodeException extends InvalidMasterListCodeExceptionBase
+{
+  private static final long serialVersionUID = -206705478;
+  
+  public InvalidMasterListCodeException()
+  {
+    super();
+  }
+  
+  public InvalidMasterListCodeException(java.lang.String developerMessage)
+  {
+    super(developerMessage);
+  }
+  
+  public InvalidMasterListCodeException(java.lang.String developerMessage, java.lang.Throwable cause)
+  {
+    super(developerMessage, cause);
+  }
+  
+  public InvalidMasterListCodeException(java.lang.Throwable cause)
+  {
+    super(cause);
+  }
+  
+}
