701ee889974023cb76fee15b75335e040c481355
terraframe <builder@terraframe.com>
2019-07-26 17:55:13 +0000

    License headers

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,22 @@
+#
+# Copyright (c) 2019 TerraFrame, Inc. All rights reserved.
+#
+# This file is part of Runway SDK(tm).
+#
+# Runway SDK(tm) is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Runway SDK(tm) is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # Replace external ips with internal ips since jenkins runs inside our VPC
 sed -i -e 's/georegistry.geoprism.net/172.31.30.53/g' geoprism-platform/ansible/inventory/georegistry/prod.ini
 sed -i -e 's/staging-georegistry.geoprism.net/172.31.23.142/g' geoprism-platform/ansible/inventory/georegistry/staging.ini
--- georegistry-web/src/main/webapp/net/geoprism/data/browser/LocationManagement.js
+++ georegistry-web/src/main/webapp/net/geoprism/data/browser/LocationManagement.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 TerraFrame, Inc. All rights reserved.
+ * Copyright (c) 2019 TerraFrame, Inc. All rights reserved.
  *
  * This file is part of Runway SDK(tm).
  *
