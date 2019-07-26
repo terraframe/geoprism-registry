57a02cea7da7331974bce6416b0bc7436d9b75e6
rrowlands <rrowlands@terraframe.com>
2019-07-26 17:15:19 -0600

    Added support for specifying version to ci script.

--- ci-build.sh
+++ ci-build.sh
@@ -23,6 +23,7 @@ sed -i -e 's/staging-georegistry.geoprism.net/172.31.23.142/g' geoprism-platform
 sed -i -e 's/dev-georegistry.geoprism.net/172.31.25.93/g' geoprism-platform/ansible/inventory/georegistry/dev.ini
 sed -i -e "s/clean_db=true/clean_db=$clean_db/g" geoprism-platform/ansible/inventory/georegistry/$environment.ini
 sed -i -e "s/clean_db=false/clean_db=$clean_db/g" geoprism-platform/ansible/inventory/georegistry/$environment.ini
+sed -i -e "s/artifact_version=.*/artifact_version=$version/g" geoprism-platform/ansible/inventory/georegistry/$environment.ini
 
 source /home/ec2-user/ansible/hacking/env-setup
 
