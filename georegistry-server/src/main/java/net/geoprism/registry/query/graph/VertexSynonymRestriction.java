88188ed846a2ebe7fed6f41af8d152f6de13779d
Justin Smethie <jsmethie@terraframe.com>
2019-12-03 13:36:57 -0700

    updated lookup to coalesce locales

--- georegistry-server/src/main/java/net/geoprism/registry/query/graph/VertexSynonymRestriction.java
+++ georegistry-server/src/main/java/net/geoprism/registry/query/graph/VertexSynonymRestriction.java
@@ -1,9 +1,18 @@
 package net.geoprism.registry.query.graph;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
+import com.runwaysdk.constants.MdAttributeLocalEmbeddedInfo;
+import com.runwaysdk.constants.MdAttributeLocalInfo;
+import com.runwaysdk.dataaccess.MdAttributeConcreteDAOIF;
 import com.runwaysdk.dataaccess.MdEdgeDAOIF;
+import com.runwaysdk.dataaccess.MdGraphClassDAOIF;
+import com.runwaysdk.dataaccess.metadata.graph.MdGraphClassDAO;
+import com.runwaysdk.session.Session;
 
 import net.geoprism.registry.model.ServerGeoObjectIF;
 import net.geoprism.registry.model.ServerHierarchyType;
@@ -17,7 +26,7 @@ public class VertexSynonymRestriction implements VertexGeoObjectRestriction
   private ServerHierarchyType hierarchyType;
 
   private Date                date;
-  
+
   public VertexSynonymRestriction(String label, Date date)
   {
     this.label = label;
@@ -44,15 +53,16 @@ public class VertexSynonymRestriction implements VertexGeoObjectRestriction
      * (uuid='85b678c2-df93-4e5b-b6c4-b264b82f9f2c'), while: ($depth < 10)}
      * RETURN location, parent ORDER BY location.code LIMIT 10
      */
-    
-//    vQuery.AND(OR.get(geQuery.getGeoId().EQ(this.label), F.TRIM(geQuery.getDisplayLabel().localize()).EQi(this.label), F.TRIM(this.localize(labelQuery)).EQi(this.label)));
-    
+
+    // vQuery.AND(OR.get(geQuery.getGeoId().EQ(this.label),
+    // F.TRIM(geQuery.getDisplayLabel().localize()).EQi(this.label),
+    // F.TRIM(this.localize(labelQuery)).EQi(this.label)));
 
     // TODO auto generate coalesce localization
     statement.append(",where: (code = :label");
-    statement.append(" OR displayLabel_cot CONTAINS (:date BETWEEN startDate AND endDate AND COALESCE(value.defaultLocale) = :label)");
+    statement.append(" OR displayLabel_cot CONTAINS (:date BETWEEN startDate AND endDate AND " + localize("value") + " = :label)");
     statement.append(" OR out('geo_vertex_has_synonym').label CONTAINS :label)");
-    
+
     parameters.put("label", this.label);
     parameters.put("date", this.date);
 
@@ -64,6 +74,48 @@ public class VertexSynonymRestriction implements VertexGeoObjectRestriction
 
       parameters.put("uuid", this.parent.getUid());
     }
+  }
+
+  public String localize(String prefix)
+  {
+    final MdGraphClassDAOIF mdLocalStruct = MdGraphClassDAO.getMdGraphClassDAO(MdAttributeLocalEmbeddedInfo.EMBEDDED_LOCAL_VALUE);
+    Locale locale = Session.getCurrentLocale();
+
+    List<String> list = new ArrayList<String>();
+
+    String localeString = locale.toString();
+
+    for (int i = localeString.length(); i > 0; i = localeString.lastIndexOf('_', i - 1))
+    {
+      String subLocale = localeString.substring(0, i);
+
+      for (MdAttributeConcreteDAOIF a : mdLocalStruct.definesAttributes())
+      {
+        if (a.definesAttribute().equalsIgnoreCase(subLocale))
+        {
+          list.add(subLocale);
+        }
+      }
+    }
+
+    list.add(MdAttributeLocalInfo.DEFAULT_LOCALE);
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("COALESCE(");
+
+    for (int i = 0; i < list.size(); i++)
+    {
+      if (i != 0)
+      {
+        builder.append(", ");
+      }
+
+      builder.append(prefix + "." + list.get(i));
+    }
+
+    builder.append(")");
+
+    return builder.toString();
   }
 
 }
