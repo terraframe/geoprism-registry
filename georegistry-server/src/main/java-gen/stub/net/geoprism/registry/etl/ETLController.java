71b43d0549ea010e70d4505f130458c269ed40fb
rrowlands <rrowlands@terraframe.com>
2020-03-02 18:16:03 -0700

    Intelligent defaults on ETLController.

--- georegistry-server/src/main/java-gen/stub/net/geoprism/registry/etl/ETLController.java
+++ georegistry-server/src/main/java-gen/stub/net/geoprism/registry/etl/ETLController.java
@@ -1,8 +1,6 @@
 package net.geoprism.registry.etl;
 
-import org.commongeoregistry.adapter.constants.RegistryUrls;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.runwaysdk.constants.ClientRequestIF;
@@ -13,6 +11,7 @@ import com.runwaysdk.mvc.ErrorSerialization;
 import com.runwaysdk.mvc.RequestParamter;
 import com.runwaysdk.mvc.ResponseIF;
 import com.runwaysdk.mvc.RestBodyResponse;
+import com.runwaysdk.system.scheduler.JobHistory;
 
 @Controller(url = "etl")
 public class ETLController
@@ -35,21 +34,41 @@ public class ETLController
   @Endpoint(method = ServletMethod.GET, error = ErrorSerialization.JSON, url = "get-active")
   public ResponseIF getActiveImports(ClientRequestIF request, @RequestParamter(name = "pageSize") Integer pageSize, @RequestParamter(name = "pageNumber") Integer pageNumber, @RequestParamter(name = "sortAttr") String sortAttr, @RequestParamter(name = "isAscending") Boolean isAscending)
   {
+    if (sortAttr == null || sortAttr == "")
+    {
+      sortAttr = JobHistory.CREATEDATE;
+    }
+    
+    if (isAscending == null)
+    {
+      isAscending = true;
+    }
+    
     JSONArray config = this.service.getActiveImports(request.getSessionId(), pageSize, pageNumber, sortAttr, isAscending);
     
     return new RestBodyResponse(config.toString());
   }
   
   @Endpoint(method = ServletMethod.GET, error = ErrorSerialization.JSON, url = "get-completed")
-  public ResponseIF getCompletedImports(String sessionId, int pageSize, int pageNumber, String sortAttr, boolean isAscending)
+  public ResponseIF getCompletedImports(String sessionId, @RequestParamter(name = "pageSize") Integer pageSize, @RequestParamter(name = "pageNumber") Integer pageNumber, @RequestParamter(name = "sortAttr") String sortAttr, @RequestParamter(name = "isAscending") Boolean isAscending)
   {
+    if (sortAttr == null || sortAttr == "")
+    {
+      sortAttr = JobHistory.CREATEDATE;
+    }
+    
+    if (isAscending == null)
+    {
+      isAscending = true;
+    }
+    
     JSONArray config = this.service.getCompletedImports(sessionId, pageSize, pageNumber, sortAttr, isAscending);
     
     return new RestBodyResponse(config.toString());
   }
   
   @Endpoint(method = ServletMethod.GET, error = ErrorSerialization.JSON, url = "get-errors")
-  public ResponseIF getImportErrors(String sessionId, @RequestParamter(name = "historyId") String historyId, @RequestParamter(name = "pageSize") int pageSize, @RequestParamter(name = "pageNumber") int pageNumber)
+  public ResponseIF getImportErrors(String sessionId, @RequestParamter(name = "historyId") String historyId, @RequestParamter(name = "pageSize") Integer pageSize, @RequestParamter(name = "pageNumber") Integer pageNumber)
   {
     JSONArray config = this.service.getImportErrors(sessionId, historyId, pageSize, pageNumber);
     
