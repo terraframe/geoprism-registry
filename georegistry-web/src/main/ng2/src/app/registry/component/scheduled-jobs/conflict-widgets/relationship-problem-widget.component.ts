cfde84d6b403d3c2ee469b1f95436bc618bb7d57
justinlewis <j.mapping@gmail.com>
2020-03-06 13:00:46 -0700

    model diff

--- georegistry-web/src/main/ng2/src/app/registry/component/scheduled-jobs/conflict-widgets/relationship-problem-widget.component.ts
+++ georegistry-web/src/main/ng2/src/app/registry/component/scheduled-jobs/conflict-widgets/relationship-problem-widget.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { BsModalService } from 'ngx-bootstrap/modal';
 import { Subject } from 'rxjs/Subject';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { GeoObjectType, MasterList, Conflict, ScheduledJob } from '../../../model/registry';
 
+import { GeoObjectEditorComponent } from '../../geoobject-editor/geoobject-editor.component';
+
 import Utils from '../../../utility/Utils'
 
 import { RegistryService } from '../../../service/registry.service';
-
 import { IOService } from '../../../service/io.service';
 import { LocalizationService } from '../../../../shared/service/localization.service';
 
@@ -31,12 +33,29 @@ export class RelationshipProblemWidgetComponent implements OnInit {
     edit: boolean = false;
 
 
-    constructor( private service: RegistryService, private iService: IOService, private lService: LocalizationService, public bsModalRef: BsModalRef ) { }
+    constructor( private service: RegistryService, private iService: IOService, 
+        private lService: LocalizationService, public bsModalRef: BsModalRef, private modalService: BsModalService
+        ) { }
 
     ngOnInit(): void {
 
         // this.onConflictAction = new Subject();
 
+    }
+
+    onConflictResolution(conflict: Conflict, job: ScheduledJob): void {
+        let editModal = this.modalService.show( GeoObjectEditorComponent, {
+            backdrop: true,
+            ignoreBackdropClick: true
+        } );
+
+        // TODO: change last param from fixed true to equivilent of this.list.isGeometryEditable
+        editModal.content.configureAsExisting( conflict.object.geoObject.attributes.code, null, null, true );
+        editModal.content.setMasterListId( null );
+        editModal.content.setOnSuccessCallback(() => {
+            // Refresh the page
+            // this.onPageChange( this.page.pageNumber );
+        } );
     }
 
     getFriendlyProblemType(type: string): string {
