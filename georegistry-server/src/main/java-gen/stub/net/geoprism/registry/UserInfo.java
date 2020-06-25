7595466a8efe7b98b8f34851047fd22484cecb74
jsmethie <jsmethie@terraframe.com>
2020-06-25 15:21:23 -0600

    Fixed bug caused by UserInfo not setting the owner to its user

--- georegistry-server/src/main/java-gen/stub/net/geoprism/registry/UserInfo.java
+++ georegistry-server/src/main/java-gen/stub/net/geoprism/registry/UserInfo.java
@@ -4,17 +4,17 @@
  * This file is part of Geoprism Registry(tm).
  *
  * Geoprism Registry(tm) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
  * Geoprism Registry(tm) is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Geoprism Registry(tm).  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Geoprism Registry(tm). If not, see <http://www.gnu.org/licenses/>.
  */
 package net.geoprism.registry;
 
@@ -57,6 +57,15 @@ public class UserInfo extends UserInfoBase
   public UserInfo()
   {
     super();
+  }
+
+  @Override
+  @Transaction
+  public void apply()
+  {
+    this.setOwner(this.getGeoprismUser());
+
+    super.apply();
   }
 
   public static JSONObject page(Integer pageSize, Integer pageNumber)
