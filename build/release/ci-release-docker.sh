542d8936484536c060274464b706d0a81cd5cd57
rrowlands <rrowlands@terraframe.com>
2020-11-10 21:20:54 -0700

    Dockerizing release process.

--- build/release/ci-release-docker.sh
+++ build/release/ci-release-docker.sh
@@ -28,8 +28,6 @@
 set -e
 set -x
 
-echo "Hello this is a test"
-
 if [ "$release_adapter" == "true" ]; then
   if curl -f -s --head "https://nexus.terraframe.com/service/local/artifact/maven/redirect?r=allrepos&g=com.cgr.adapter&a=cgradapter-common&p=jar&v=$CGR_RELEASE_VERSION" | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null; then
     echo "The release version $CGR_RELEASE_VERSION has already been deployed! Please ensure you are releasing the correct version."
@@ -38,10 +36,11 @@ if [ "$release_adapter" == "true" ]; then
 fi
 
 export WORKSPACE=/workspace
-export ANDROID_HOME=/opt/android-sdk
 
 git config --global user.name "$GIT_TF_BUILDER_USERNAME"
 git config --global user.email builder@terraframe.com
+
+. $NVM_DIR/nvm.sh && nvm instal lts/erbium
 
 if [ "$release_adapter" == "true" ]; then
   #### CGR Adapter ####
