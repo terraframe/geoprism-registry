ceb9c4996896ea1af9d88fd1011efad3c3749d3c
builder@terraframe.com <builder@terraframe.com>
2020-11-11 04:33:42 +0000

    License headers

--- georegistry-server/src/main/java/net/geoprism/registry/GeoregistryConfigGroup.java
+++ georegistry-server/src/main/java/net/geoprism/registry/GeoregistryConfigGroup.java
@@ -1,20 +1,20 @@
 /**
- * Copyright (c) 2015 TerraFrame, Inc. All rights reserved.
+ * Copyright (c) 2019 TerraFrame, Inc. All rights reserved.
  *
- * This file is part of Geoprism(tm).
+ * This file is part of Geoprism Registry(tm).
  *
- * Geoprism(tm) is free software: you can redistribute it and/or modify
+ * Geoprism Registry(tm) is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * Geoprism(tm) is distributed in the hope that it will be useful, but
+ * Geoprism Registry(tm) is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Geoprism(tm).  If not, see <http://www.gnu.org/licenses/>.
+ * License along with Geoprism Registry(tm).  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.geoprism.registry;
 
