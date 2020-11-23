459b8cf37337d8c15c26a5acca8888255f0a72cc
jsmethie <jsmethie@terraframe.com>
2020-11-23 13:17:37 -0700

    RA/RM should be able to specify the date from which the publishing of a list takes place #265

--- georegistry-server/src/main/java-gen/stub/net/geoprism/registry/MasterList.java
+++ georegistry-server/src/main/java-gen/stub/net/geoprism/registry/MasterList.java
@@ -4,17 +4,17 @@
  * This file is part of Geoprism Registry(tm).
  *
  * Geoprism Registry(tm) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
  * Geoprism Registry(tm) is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Geoprism Registry(tm).  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Geoprism Registry(tm). If not, see <http://www.gnu.org/licenses/>.
  */
 package net.geoprism.registry;
 
@@ -565,12 +565,10 @@ public class MasterList extends MasterListBase
   private Pair<Date, Date> getDateRange(final ServerGeoObjectType objectType)
   {
     Pair<Date, Date> range = VertexServerGeoObject.getDataRange(objectType);
-
     if (this.getPublishingStartDate() != null)
     {
-      return new Pair<Date, Date>(this.getPublishDate(), range.getSecond());
+      return new Pair<Date, Date>(this.getPublishingStartDate(), range.getSecond());
     }
-
     return range;
   }
 
