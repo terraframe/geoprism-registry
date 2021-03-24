482e4d2d76133483a2d0fb95745505ba93ede927
rrowlands <rrowlands@terraframe.com>
2021-03-24 16:35:03 -0600

    More whitespace stripping.

--- georegistry-server/src/main/java/net/geoprism/registry/model/graph/VertexServerGeoObject.java
+++ georegistry-server/src/main/java/net/geoprism/registry/model/graph/VertexServerGeoObject.java
@@ -213,6 +213,7 @@ public class VertexServerGeoObject extends AbstractServerGeoObject implements Se
   public void setCode(String code)
   {
     code = code.trim();
+    code = code.replaceAll("\\s+","");
     
     this.vertex.setValue(GeoVertex.GEOID, code);
     this.vertex.setValue(DefaultAttribute.CODE.getName(), code);
