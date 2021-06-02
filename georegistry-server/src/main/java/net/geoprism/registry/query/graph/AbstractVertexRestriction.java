714d3aa0960a28854bf4ab762626ffd89433ea75
rrowlands <rrowlands@terraframe.com>
2021-06-02 17:05:32 -0600

    Searching for Geo-Objects from the Explorer doesn't work for the Laos locale #623

--- georegistry-server/src/main/java/net/geoprism/registry/query/graph/AbstractVertexRestriction.java
+++ georegistry-server/src/main/java/net/geoprism/registry/query/graph/AbstractVertexRestriction.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import com.runwaysdk.LocalizationFacade;
 import com.runwaysdk.constants.MdAttributeLocalEmbeddedInfo;
 import com.runwaysdk.constants.MdAttributeLocalInfo;
 import com.runwaysdk.dataaccess.MdAttributeConcreteDAOIF;
@@ -31,27 +32,26 @@ import com.runwaysdk.session.Session;
 
 public class AbstractVertexRestriction
 {
-  public String localize(String prefix)
+  public static String localize(String prefix)
   {
     final MdGraphClassDAOIF mdLocalStruct = MdGraphClassDAO.getMdGraphClassDAO(MdAttributeLocalEmbeddedInfo.EMBEDDED_LOCAL_VALUE);
-    Locale locale = Session.getCurrentLocale();
 
     List<String> list = new ArrayList<String>();
 
-    String localeString = locale.toString();
-
-    for (int i = localeString.length(); i > 0; i = localeString.lastIndexOf('_', i - 1))
-    {
-      String subLocale = localeString.substring(0, i);
-
-      for (MdAttributeConcreteDAOIF a : mdLocalStruct.definesAttributes())
-      {
-        if (a.definesAttribute().equalsIgnoreCase(subLocale))
-        {
-          list.add(subLocale);
-        }
-      }
-    }
+//    if (!allLocales)
+//    {
+      Locale locale = Session.getCurrentLocale();
+      addLocale(mdLocalStruct, locale, list);
+//    }
+//    else
+//    {
+//      List<Locale> locales = LocalizationFacade.getInstalledLocales();
+//      
+//      for (Locale locale : locales)
+//      {
+//        addLocale(mdLocalStruct, locale, list);
+//      }
+//    }
 
     list.add(MdAttributeLocalInfo.DEFAULT_LOCALE);
 
@@ -73,4 +73,21 @@ public class AbstractVertexRestriction
     return builder.toString();
   }
 
+  private static void addLocale(final MdGraphClassDAOIF mdLocalStruct, Locale locale, List<String> list)
+  {
+    String localeString = locale.toString();
+
+    for (int i = localeString.length(); i > 0; i = localeString.lastIndexOf('_', i - 1))
+    {
+      String subLocale = localeString.substring(0, i);
+
+      for (MdAttributeConcreteDAOIF a : mdLocalStruct.definesAttributes())
+      {
+        if (a.definesAttribute().equalsIgnoreCase(subLocale))
+        {
+          list.add(subLocale.toLowerCase());
+        }
+      }
+    }
+  }
 }
