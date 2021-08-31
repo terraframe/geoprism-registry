75f50db9a506d1e372faadaf587d07eb0751562d
rrowlands <rrowlands@terraframe.com>
2021-08-31 12:51:50 -0600

    Fix issue where forgot password complete page wasn't loading.

--- georegistry-web/src/main/ng2/src/app/core/component/forgotpassword-complete/forgotpassword-complete.component.ts
+++ georegistry-web/src/main/ng2/src/app/core/component/forgotpassword-complete/forgotpassword-complete.component.ts
@@ -17,61 +17,59 @@
 /// License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
 ///
 
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 import { HttpErrorResponse } from "@angular/common/http";
 
-import { ErrorHandler } from '@shared/component';
+import { ErrorHandler } from "@shared/component";
 
-import { ForgotPasswordService } from '@core/service/forgotpassword.service';
-import { PasswordStrengthBarComponent } from '@shared/component/password-strength-bar/password-strength-bar.component';
-
-
+import { ForgotPasswordService } from "@core/service/forgotpassword.service";
+import { PasswordStrengthBarComponent } from "@shared/component/password-strength-bar/password-strength-bar.component";
 
 @Component({
-  templateUrl: './forgotpassword-complete.component.html',
-  styleUrls: ['./forgotpassword-complete.component.css']
+    templateUrl: "./forgotpassword-complete.component.html",
+    styleUrls: ["./forgotpassword-complete.component.css"]
 })
 export class ForgotPasswordCompleteComponent implements OnInit {
-  newPassword: string;
-  token: string;
-  passwordIsReset: boolean = false;
-  private sub: any;
-  message: string = null;
 
-  constructor(
-    private service: ForgotPasswordService,
-    private router: Router,      
-    private route: ActivatedRoute,
-	private passwordStrengthBarComponent: PasswordStrengthBarComponent) {
-  }
-  
-  ngOnInit() {
-    this.sub = this.route.params.subscribe(params => {
-       this.token = params['token'];
-    });
-  }
+    newPassword: string;
+    token: string;
+    passwordIsReset: boolean = false;
+    private sub: any;
+    message: string = null;
 
-  ngOnDestroy() {
-    this.sub.unsubscribe();
-  }
-  
-  
-  cancel(): void {    
-    this.router.navigate(['/']);
-  } 
-  
-  onSubmit(): void {
-    this.service.complete(this.newPassword, this.token)
-      .then(() => {
-        this.passwordIsReset = true;
-      })
-      .catch(( err: HttpErrorResponse ) => {
-        this.error( err );
-      } );
-  }
-  
-  error( err: HttpErrorResponse ): void {
-      this.message = ErrorHandler.getMessageFromError(err);
-  }
+    constructor(
+      private service: ForgotPasswordService,
+      private router: Router,      
+      private route: ActivatedRoute,
+  	  private passwordStrengthBarComponent: PasswordStrengthBarComponent) {}
+
+    ngOnInit() {
+        this.sub = this.route.params.subscribe(params => {
+            this.token = params["token"];
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    cancel(): void {
+        this.router.navigate(["/"]);
+    }
+
+    onSubmit(): void {
+        this.service.complete(this.newPassword, this.token)
+            .then(() => {
+                this.passwordIsReset = true;
+            })
+            .catch((err: HttpErrorResponse) => {
+                this.error(err);
+            });
+    }
+
+    error(err: HttpErrorResponse): void {
+        this.message = ErrorHandler.getMessageFromError(err);
+    }
+
 }
