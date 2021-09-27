268d3affdac2e84c8310ae0bf7c6ab47421a3c96
rrowlands <rrowlands@terraframe.com>
2021-09-27 15:00:53 -0600

    Importing localization results in unspecified error #670

---
+++ georegistry-web/src/main/ng2/src/app/admin/component/localization-manager/localization-feedback-modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from "@angular/core";
+import { BsModalRef } from "ngx-bootstrap/modal";
+import { LocalizationService } from "@shared/service";
+
+@Component({
+    selector: "error-modal",
+    templateUrl: "./localization-feedback-modal.component.html",
+    styleUrls: ["./localization-feedback-modal.css"]
+})
+export class LocalizationFeedbackModalComponent {
+
+    /*
+     * Message
+     */
+    @Input() message: string = this.localizeService.decode("error.modal.default.message");
+
+    // eslint-disable-next-line no-useless-constructor
+    constructor(public bsModalRef: BsModalRef, private localizeService: LocalizationService) { }
+
+}
