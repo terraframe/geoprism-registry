cdd028d5ad0e8004fb34ac58499d556d02eb0f4a
jsmethie <jsmethie@terraframe.com>
2021-09-29 14:37:21 -0600

    Updated BasicFhirResourceProcessor to use Location.position for geometry information on MultiPoint geometry types

--- georegistry-server/src/main/java/net/geoprism/registry/etl/fhir/BasicFhirResourceProcessor.java
+++ georegistry-server/src/main/java/net/geoprism/registry/etl/fhir/BasicFhirResourceProcessor.java
@@ -1,19 +1,36 @@
 package net.geoprism.registry.etl.fhir;
 
+import java.math.BigDecimal;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Optional;
+import java.util.Base64.Decoder;
 
+import org.commongeoregistry.adapter.constants.GeometryType;
 import org.commongeoregistry.adapter.dataaccess.LocalizedValue;
+import org.hl7.fhir.r4.model.Attachment;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Location;
+import org.hl7.fhir.r4.model.Location.LocationPositionComponent;
 import org.hl7.fhir.r4.model.Organization;
 
 import com.runwaysdk.dataaccess.ProgrammingErrorException;
 import com.runwaysdk.dataaccess.graph.attributes.ValueOverTime;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.PrecisionModel;
+import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.geojson.GeoJsonReader;
 
 import net.geoprism.registry.model.ServerGeoObjectIF;
+import net.geoprism.registry.model.ServerGeoObjectType;
 
 public class BasicFhirResourceProcessor extends AbstractFhirResourceProcessor implements FhirResourceProcessor
 {
@@ -87,4 +104,29 @@ public class BasicFhirResourceProcessor extends AbstractFhirResourceProcessor im
     return location.getIdentifier().stream().filter(i -> i.getSystem().equals(this.getSystem().getSystem())).findFirst().orElse(null);
   }
 
+  @Override
+  protected Geometry getGeometry(Location location, ServerGeoObjectType type)
+  {
+    if (type.getGeometryType().equals(GeometryType.MULTIPOINT))
+    {
+      LocationPositionComponent position = location.getPosition();
+
+      if (position != null)
+      {
+        GeometryFactory factory = new GeometryFactory(new PrecisionModel(), 4326);
+
+        BigDecimal latitude = position.getLatitude();
+        BigDecimal longitude = position.getLongitude();
+
+        if (latitude != null && longitude != null)
+        {
+          Point point = factory.createPoint(new Coordinate(longitude.doubleValue(), latitude.doubleValue()));
+
+          return factory.createMultiPoint(new Point[] { point });
+        }
+      }
+    }
+
+    return super.getGeometry(location, type);
+  }
 }
