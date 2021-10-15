306a7637bf5a7352576f191e0b95158fb7a3f108
rrowlands <rrowlands@terraframe.com>
2021-10-15 16:47:54 -0600

    Dockerizing

--- src/build/docker/georegistry/build.sh
+++ src/build/docker/georegistry/build.sh
@@ -1,5 +1,7 @@
 ([ -d target ] && rm -rf target) || true
 mkdir target
 cp ../../../../georegistry-web/target/georegistry.war target/georegistry.war
+cp -R ../../../../envcfg/prod target/appcfg
 
 docker build -t georegistry .
+docker save georegistry:latest | gzip > target/georegistry.dimg.gz
