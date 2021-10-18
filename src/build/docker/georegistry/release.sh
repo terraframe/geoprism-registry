a5fe05d60d5953bf1614351114d10bc85c28ec9d
rrowlands <rrowlands@terraframe.com>
2021-10-18 14:48:53 -0600

    Fix compile error.

--- src/build/docker/georegistry/release.sh
+++ src/build/docker/georegistry/release.sh
@@ -7,6 +7,6 @@ docker build -t georegistry .
 
 docker tag georegistry:latest terraframe/geoprism-registry:latest
 
-echo $DOCKER_HUB_TFBUILDER_PASSWORD | docker login --username $DOCKER_HUB_TFBUILDER_USERNAME --password-stdin terraframe/geoprism-registry
+echo $DOCKER_HUB_TFBUILDER_PSW | docker login --username $DOCKER_HUB_TFBUILDER_USR --password-stdin terraframe/geoprism-registry
 
 docker push terraframe/geoprism-registry:latest
