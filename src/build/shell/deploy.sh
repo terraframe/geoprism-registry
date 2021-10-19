3ec121ab56e67b7046d6e37c163cd9fbc21ec67d
rrowlands <rrowlands@terraframe.com>
2021-10-19 09:26:30 -0600

    Dockerizing

--- src/build/shell/deploy.sh
+++ src/build/shell/deploy.sh
@@ -69,7 +69,7 @@ if [ "$build_artifact" == "true" ]; then
   cd $WORKSPACE/georegistry
   mvn clean deploy -B -Djavax.net.ssl.trustStore=$WORKSPACE/georegistry/georegistry-web/src/test/resources/tomcat.truststore -Djavax.net.ssl.trustStorePassword=2v8hVW2rPFncN6m -Djavax.net.ssl.keyStore=$WORKSPACE/georegistry/georegistry-web/src/test/resources/keystore.ks -Djavax.net.ssl.keyStorePassword=2v8hVW2rPFncN6m
 else
-  wget -nv -O $WORKSPACE/georegistry/georegistry-web/target/georegistry.war "http://nexus.terraframe.com/service/local/artifact/maven/redirect?r=allrepos&g=net.geoprism&a=georegistry-web&p=war&v=$tag"
+  mkdir -p $WORKSPACE/georegistry/georegistry-web/target && wget -nv -O $WORKSPACE/georegistry/georegistry-web/target/georegistry.war "http://nexus.terraframe.com/service/local/artifact/maven/redirect?r=allrepos&g=net.geoprism&a=georegistry-web&p=war&v=$tag"
 fi
 
 # Build a Docker image
