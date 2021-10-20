44ef30436ee9957d43c16e4b99e34ab0e66f94b2
rrowlands <rrowlands@terraframe.com>
2021-10-20 11:00:09 -0600

    Improving docker release process.

--- src/build/docker/georegistry/build.sh
+++ src/build/docker/georegistry/build.sh
@@ -8,5 +8,5 @@ mkdir target
 cp ../../../../georegistry-web/target/georegistry.war target/georegistry.war
 cp -R ../../../../envcfg/prod target/appcfg
 
-docker build -t geoprism-registry .
-docker save geoprism-registry:latest | gzip > target/georegistry.dimg.gz
+docker build -t terraframe/geoprism-registry:latest .
+docker save terraframe/geoprism-registry:latest | gzip > target/georegistry.dimg.gz
--- src/build/docker/georegistry/release.sh
+++ src/build/docker/georegistry/release.sh
@@ -3,8 +3,8 @@
 
 set -e
 
-docker tag geoprism-registry:latest terraframe/geoprism-registry:latest
+docker tag terraframe/geoprism-registry:latest terraframe/geoprism-registry:$CGR_RELEASE_VERSION
 
 echo $DOCKER_HUB_TFBUILDER_PSW | docker login --username $DOCKER_HUB_TFBUILDER_USR --password-stdin
 
-docker push terraframe/geoprism-registry:latest
+docker push terraframe/geoprism-registry --all-tags
