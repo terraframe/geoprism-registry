74aadfe3a3f5cacb161e44dae4877b2046b56fde
rrowlands <rrowlands@terraframe.com>
2021-11-30 09:51:45 -0700

    Add column header mapping to metadata for Shapefile exports #628

--- georegistry-test/src/test/java/net/geoprism/registry/transition/TransitionEventTest.java
+++ georegistry-test/src/test/java/net/geoprism/registry/transition/TransitionEventTest.java
@@ -193,7 +193,7 @@ public class TransitionEventTest
       event.setAfterTypeOrgCode(FastTestDataset.PROVINCE.getOrganization().getCode());
       event.apply();
 
-      Transition transition = event.addTransition(FastTestDataset.CAMBODIA.getServerObject(), FastTestDataset.PROV_CENTRAL.getServerObject(), TransitionType.DOWNGRADE_REASSIGN, TransitionImpact.FULL);
+      Transition transition = event.addTransition(FastTestDataset.CAMBODIA.getServerObject(), FastTestDataset.PROV_CENTRAL.getServerObject(), TransitionType.DOWNGRADE, TransitionImpact.FULL);
 
       Assert.assertEquals(6, Task.getTasks(transition.getOid()).size());
 
@@ -455,10 +455,10 @@ public class TransitionEventTest
       Assert.assertEquals("Event Date", header.getCell(1).getStringCellValue());
       Assert.assertEquals("Event Type", header.getCell(2).getStringCellValue());
       Assert.assertEquals("Description", header.getCell(3).getStringCellValue());
-      Assert.assertEquals("Before Type", header.getCell(4).getStringCellValue());
+      Assert.assertEquals("Before Type Code", header.getCell(4).getStringCellValue());
       Assert.assertEquals("Before Code", header.getCell(5).getStringCellValue());
       Assert.assertEquals("Before Label", header.getCell(6).getStringCellValue());
-      Assert.assertEquals("After Type", header.getCell(7).getStringCellValue());
+      Assert.assertEquals("After Type Code", header.getCell(7).getStringCellValue());
       Assert.assertEquals("After Code", header.getCell(8).getStringCellValue());
       Assert.assertEquals("After Label", header.getCell(9).getStringCellValue());
 
