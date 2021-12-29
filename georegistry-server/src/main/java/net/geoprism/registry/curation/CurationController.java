5d2d5896fe93918e2aee3129a564038f1488ec4f
rrowlands <rrowlands@terraframe.com>
2021-12-29 15:00:49 -0700

    Switched curation endpoints to GET from POST.

--- georegistry-server/src/main/java/net/geoprism/registry/curation/CurationController.java
+++ georegistry-server/src/main/java/net/geoprism/registry/curation/CurationController.java
@@ -20,7 +20,7 @@ public class CurationController
     this.service = new CurationService();
   }
   
-  @Endpoint(method = ServletMethod.POST, error = ErrorSerialization.JSON, url = "details")
+  @Endpoint(method = ServletMethod.GET, error = ErrorSerialization.JSON, url = "details")
   public ResponseIF details(ClientRequestIF request, @RequestParamter(name = "historyId") String historyId, @RequestParamter(name = "onlyUnresolved") Boolean onlyUnresolved, @RequestParamter(name = "pageSize") Integer pageSize, @RequestParamter(name = "pageNumber") Integer pageNumber)
   {
     JsonObject details = this.service.details(request.getSessionId(), historyId, onlyUnresolved, pageSize, pageNumber);
@@ -28,7 +28,7 @@ public class CurationController
     return new RestBodyResponse(details.toString());
   }
   
-  @Endpoint(method = ServletMethod.POST, error = ErrorSerialization.JSON, url = "page")
+  @Endpoint(method = ServletMethod.GET, error = ErrorSerialization.JSON, url = "page")
   public ResponseIF page(ClientRequestIF request, @RequestParamter(name = "historyId") String historyId, @RequestParamter(name = "onlyUnresolved") Boolean onlyUnresolved, @RequestParamter(name = "pageSize") Integer pageSize, @RequestParamter(name = "pageNumber") Integer pageNumber)
   {
     JsonObject page = this.service.page(request.getSessionId(), historyId, onlyUnresolved, pageSize, pageNumber);
