b9b0f474c8c89fec328bd9de654f35736c9b9731
jsmethie <jsmethie@terraframe.com>
2022-01-14 10:39:33 -0700

    Fixed issue with date filtering in the graph page query

--- georegistry-server/src/main/java/net/geoprism/registry/query/graph/AbstractGraphPageQuery.java
+++ georegistry-server/src/main/java/net/geoprism/registry/query/graph/AbstractGraphPageQuery.java
@@ -53,74 +53,87 @@ public abstract class AbstractGraphPageQuery<K, T extends JsonSerializable>
     if (criteria.has("filters"))
     {
       JsonObject filters = criteria.get("filters").getAsJsonObject();
-      Iterator<String> keys = filters.keySet().iterator();
 
-      int i = 0;
-
-      while (keys.hasNext())
-      {
-        String attributeName = keys.next();
-
-        MdAttributeDAOIF mdAttribute = mdVertex.definesAttribute(attributeName);
-
-        if (mdAttribute != null)
-        {
-          String columnName = this.getColumnName(mdAttribute);
-
-          JsonObject filter = filters.get(attributeName).getAsJsonObject();
-
-          String mode = filter.get("matchMode").getAsString();
-
-          if (mode.equals("between"))
-          {
-            JsonObject value = filter.get("value").getAsJsonObject();
-
-            if (value.has("startDate") && !value.get("startDate").isJsonNull())
-            {
-              String date = value.get("startDate").getAsString();
-
-              if (date.length() > 0)
-              {
-                parameters.put(attributeName + "StartDate", GeoRegistryUtil.parseDate(date));
-
-                statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " >= :" + attributeName + "StartDate");
-              }
-            }
-
-            if (value.has("endDate") && !value.get("endDate").isJsonNull())
-            {
-              String date = value.get("endDate").getAsString();
-
-              if (date.length() > 0)
-              {
-                parameters.put(attributeName + "EndDate", GeoRegistryUtil.parseDate(date));
-
-                statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " <= :" + attributeName + "EndDate");
-              }
-            }
-
-          }
-          else if (mode.equals("contains"))
-          {
-            parameters.put(attributeName, "%" + filter.get("value").getAsString().toUpperCase() + "%");
-
-            statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + ".toUpperCase() LIKE :" + attributeName);
-          }
-          else if (mode.equals("equals"))
-          {
-            parameters.put(attributeName, filter.get("value").getAsString());
-
-            statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " = :" + attributeName);
-          }
-
-          i++;
-        }
-      }
+      this.addCriteria(mdVertex, filters, statement, parameters);
     }
 
     final GraphQuery<Long> query = new GraphQuery<Long>(statement.toString(), parameters);
 
     return query.getSingleResult();
+  }
+
+  private void addCriteria(final MdVertexDAOIF mdVertex, JsonObject filters, StringBuilder statement, Map<String, Object> parameters)
+  {
+    Iterator<String> keys = filters.keySet().iterator();
+
+    int i = 0;
+
+    while (keys.hasNext())
+    {
+      String attributeName = keys.next();
+
+      MdAttributeDAOIF mdAttribute = mdVertex.definesAttribute(attributeName);
+
+      if (mdAttribute != null)
+      {
+        String columnName = this.getColumnName(mdAttribute);
+
+        JsonObject filter = filters.get(attributeName).getAsJsonObject();
+
+        String mode = filter.get("matchMode").getAsString();
+
+        if (mode.equals("between"))
+        {
+          JsonObject value = filter.get("value").getAsJsonObject();
+
+          if (value.has("startDate") && !value.get("startDate").isJsonNull())
+          {
+            String date = value.get("startDate").getAsString();
+
+            if (date.length() > 0)
+            {
+              parameters.put(attributeName + "StartDate", GeoRegistryUtil.parseDate(date));
+
+              statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " >= :" + attributeName + "StartDate");
+
+              i++;
+            }
+          }
+
+          if (value.has("endDate") && !value.get("endDate").isJsonNull())
+          {
+            String date = value.get("endDate").getAsString();
+
+            if (date.length() > 0)
+            {
+              parameters.put(attributeName + "EndDate", GeoRegistryUtil.parseDate(date));
+
+              statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " <= :" + attributeName + "EndDate");
+
+              i++;
+            }
+          }
+
+        }
+        else if (mode.equals("contains"))
+        {
+          parameters.put(attributeName, "%" + filter.get("value").getAsString().toUpperCase() + "%");
+
+          statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + ".toUpperCase() LIKE :" + attributeName);
+
+          i++;
+        }
+        else if (mode.equals("equals"))
+        {
+          parameters.put(attributeName, filter.get("value").getAsString());
+
+          statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " = :" + attributeName);
+
+          i++;
+        }
+
+      }
+    }
   }
 
   public Page<T> getPage()
@@ -142,69 +155,8 @@ public abstract class AbstractGraphPageQuery<K, T extends JsonSerializable>
     if (criteria.has("filters"))
     {
       JsonObject filters = criteria.get("filters").getAsJsonObject();
-      Iterator<String> keys = filters.keySet().iterator();
 
-      int i = 0;
-
-      while (keys.hasNext())
-      {
-        String attributeName = keys.next();
-
-        MdAttributeDAOIF mdAttribute = mdVertex.definesAttribute(attributeName);
-
-        if (mdAttribute != null)
-        {
-          String columnName = this.getColumnName(mdAttribute);
-
-          JsonObject filter = filters.get(attributeName).getAsJsonObject();
-
-          String mode = filter.get("matchMode").getAsString();
-
-          if (mode.equals("between"))
-          {
-            JsonObject value = filter.get("value").getAsJsonObject();
-
-            if (value.has("startDate") && !value.get("startDate").isJsonNull())
-            {
-              String date = value.get("startDate").getAsString();
-
-              if (date.length() > 0)
-              {
-                parameters.put(attributeName + "StartDate", GeoRegistryUtil.parseDate(date));
-
-                statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " >= :" + attributeName + "StartDate");
-              }
-            }
-
-            if (value.has("endDate") && !value.get("endDate").isJsonNull())
-            {
-              String date = value.get("endDate").getAsString();
-
-              if (date.length() > 0)
-              {
-                parameters.put(attributeName + "EndDate", GeoRegistryUtil.parseDate(date));
-
-                statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " <= :" + attributeName + "EndDate");
-              }
-            }
-
-          }
-          else if (mode.equals("contains"))
-          {
-            parameters.put(attributeName, "%" + filter.get("value").getAsString().toUpperCase() + "%");
-
-            statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + ".toUpperCase() LIKE :" + attributeName);
-          }
-          else if (mode.equals("equals"))
-          {
-            parameters.put(attributeName, filter.get("value").getAsString());
-
-            statement.append( ( ( i == 0 ) ? " WHERE " : " AND " ) + columnName + " = :" + attributeName);
-          }
-
-          i++;
-        }
-      }
+      this.addCriteria(mdVertex, filters, statement, parameters);
     }
 
     if (criteria.has("sortField") && criteria.has("sortOrder"))
