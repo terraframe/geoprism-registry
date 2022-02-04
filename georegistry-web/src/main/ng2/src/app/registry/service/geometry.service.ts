02904a878ae341b71fa3cc34622c3df1cf0f003b
rrowlands <rrowlands@terraframe.com>
2022-02-04 13:36:34 -0700

    Fix issue where the geometry editor was throwing errors when constructing.

--- georegistry-web/src/main/ng2/src/app/registry/service/geometry.service.ts
+++ georegistry-web/src/main/ng2/src/app/registry/service/geometry.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Output, EventEmitter } from "@angular/core";
 
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import * as MapboxDraw from "@mapbox/mapbox-gl-draw";
 import { Map, LngLat, LngLatBounds } from "mapbox-gl";
 
 import { Layer } from "../component/geoobject-shared-attribute-editor/manage-versions-model";
@@ -252,6 +252,7 @@ export class GeometryService {
     enableEditing(): void {
         if (this.editingControl == null) {
             if (this.geometryType === "MULTIPOLYGON" || this.geometryType === "POLYGON") {
+                console.log(MapboxDraw);
                 this.editingControl = new MapboxDraw({
                     controls: {
                         point: false,
