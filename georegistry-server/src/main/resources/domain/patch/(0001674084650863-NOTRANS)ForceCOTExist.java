4443c70dc23b94d091221d04d9d638417ec7b6d0
Richard Rowlands <rrowlands@terraframe.com>
2023-01-25 16:11:09 -0700

    chore: fix issue with orientdb v3.2 patcher

--- georegistry-server/src/main/resources/domain/patch/(0001674084650863-NOTRANS)ForceCOTExist.java
+++
@@ -1,9 +0,0 @@
-<version xsi:noNamespaceSchemaLocation="classpath:com/runwaysdk/resources/xsd/version.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <doIt>
-    <create></create>
-    <update></update>
-  </doIt>
-  <undoIt>
-    <delete></delete>
-  </undoIt>
-</version>
