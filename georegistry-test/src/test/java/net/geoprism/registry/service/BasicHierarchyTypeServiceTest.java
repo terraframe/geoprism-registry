5a659198484b92815ef28769b51b8bdce442b996
jsmethie <jsmethie@terraframe.com>
2024-02-26 14:57:33 -0700

    chore: graph metadata refactor ability to assign geo object types to a hierarchy

--- georegistry-test/src/test/java/net/geoprism/registry/service/BasicHierarchyTypeServiceTest.java
+++ georegistry-test/src/test/java/net/geoprism/registry/service/BasicHierarchyTypeServiceTest.java
@@ -3,17 +3,21 @@
  */
 package net.geoprism.registry.service;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
+import com.google.gson.JsonArray;
 import com.runwaysdk.session.Request;
 
 import net.geoprism.registry.InstanceTestClassListener;
 import net.geoprism.registry.SpringInstanceTestClassRunner;
 import net.geoprism.registry.TestConfig;
+import net.geoprism.registry.model.RootGeoObjectType;
 import net.geoprism.registry.model.ServerGeoObjectType;
 import net.geoprism.registry.model.ServerHierarchyType;
 import net.geoprism.registry.service.business.GeoObjectTypeBusinessServiceIF;
@@ -70,6 +74,54 @@ public class BasicHierarchyTypeServiceTest implements InstanceTestClassListener
       ServerHierarchyType test = this.service.get(hierarchyType.getCode());
 
       Assert.assertNotNull(test);
+    }
+    finally
+    {
+      this.service.delete(hierarchyType);
+    }
+  }
+
+  @Test
+  @Request
+  public void testAddChildToRoot()
+  {
+    ServerHierarchyType hierarchyType = this.service.createHierarchyType(USATestData.HIER_ADMIN.toDTO());
+
+    try
+    {
+      this.service.addToHierarchy(hierarchyType, RootGeoObjectType.INSTANCE, parent);
+
+      List<ServerGeoObjectType> roots = this.service.getDirectRootNodes(hierarchyType);
+
+      Assert.assertEquals(1, roots.size());
+    }
+    finally
+    {
+      this.service.delete(hierarchyType);
+    }
+  }
+
+  @Test
+  @Request
+  public void testAddGrandChild()
+  {
+    ServerHierarchyType hierarchyType = this.service.createHierarchyType(USATestData.HIER_ADMIN.toDTO());
+
+    try
+    {
+      this.service.addToHierarchy(hierarchyType, RootGeoObjectType.INSTANCE, parent);
+      this.service.addToHierarchy(hierarchyType, parent, child);
+      this.service.addToHierarchy(hierarchyType, child, grandChild);
+
+      List<ServerGeoObjectType> children = this.service.getChildren(hierarchyType, parent);
+
+      Assert.assertEquals(1, children.size());
+
+      Assert.assertEquals(child, children.get(0));
+
+      JsonArray results = this.service.getHierarchiesForType(grandChild.getCode(), false);
+
+      Assert.assertEquals(1, results.size());
     }
     finally
     {
