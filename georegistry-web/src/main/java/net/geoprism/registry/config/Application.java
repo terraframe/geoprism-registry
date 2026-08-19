05d670db84386493d54ca1a7b81d6174c008189b
Richard Rowlands <rrowlands@terraframe.com>
2026-08-19 11:15:38 -0600

    Fix issue with database bootstrapping

--- georegistry-web/src/main/java/net/geoprism/registry/config/Application.java
+++ georegistry-web/src/main/java/net/geoprism/registry/config/Application.java
@@ -1,9 +1,15 @@
 package net.geoprism.registry.config;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.runwaysdk.constants.DeployProperties;
+
+import net.geoprism.PluginUtil;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer
@@ -17,6 +23,16 @@ public class Application extends SpringBootServletInitializer
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
   {
+    buildDb();
+    
     return application.sources(SpringAppConfig.class);
+  }
+  
+  private void buildDb()
+  {
+    var patcher = PluginUtil.getDatabaseBuilder();
+
+    patcher.initialize(new File(DeployProperties.getDeployBin(), "metadata"));
+    patcher.run();
   }
 }
